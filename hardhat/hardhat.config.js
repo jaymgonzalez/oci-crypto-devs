07f2474407a97d1556ec09a1f2e907af10b3775c
Jay M Gonzalez <jaimungon@gmail.com>
2022-10-26 11:03:53 +0200

    add config parameters

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config({ path: '.env' })
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.4',
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+}
