9b093827b73ecd7d4688d5313b3b0e29c333b27d
Jay M Gonzalez <jaimungon@gmail.com>
2022-10-26 11:02:03 +0200

    add nft contract address abi

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  '0xc1bb58dbf8F1632E1C4BA4a47aA90afB9a335cD4'
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS }
